60b4fef281ccc7337d0fb36c251c4fe97049f707
Venkata Pavan <138016465+venkatapavan2905@users.noreply.github.com>
2025-03-01 18:34:58 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+    backend "s3" {
+        bucket = "terraform-state-bucket-2906"
+        key = "global/terraform.tfstate"
+        region = "ap-south-1"
+        encrypt = true
+        dynamodb_table = "terraform-lock"
+    }
+}
